782a8839dd5f6bca5cb88df92831006b8119a075
Betül Tuğçe Dikdoğmuş <betultugcedikdogmus@gmail.com>
2025-05-19 13:26:15 +0300

    ConnectionString yazildi

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -5,5 +5,9 @@ namespace API.Data;
 
 public class DataContext : DbContext
 {
+    public DataContext(DbContextOptions options): base(options)
+    {
+        
+    }
     public DbSet<Product> Products { get; set; }
 }
